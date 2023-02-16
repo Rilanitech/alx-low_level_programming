bc0f16b18b40271ef43b2964a7a546e85440f4cb
Rilanitech <ilanirejoice50@gmail.com>
2023-02-16 12:53:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A function that execution the code
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
