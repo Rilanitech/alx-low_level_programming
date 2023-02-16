c391033cc8152bdfbb0b5f3340a22367506b3d06
Rilanitech <ilanirejoice50@gmail.com>
2023-02-16 12:28:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
-
 /**
  * main - The main function serves as the starting point for code execution
- * 
- * Return: Always 0 (Success)
- */
-
-
+ * Return:0(Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+	return(0);
 }
