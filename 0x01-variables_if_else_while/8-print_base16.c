c886b408f0be2c298cadb87566ac2e60de0cdf0d
Rilanitech <ilanirejoice50@gmail.com>
2023-02-17 15:21:39 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{	int i;
+	char p;
+
+	for (i = '0' ; i < 10 ; i++)
+		putchar(i + '0');
+	for (p = 'a' ; p <= 'f' ; p++)
+		putchar(p);
+	putchar('\n');
+	return (0);
+}
