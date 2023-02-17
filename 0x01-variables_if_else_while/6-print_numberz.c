6d3a6f46c0be0b8a6e13e08202ef1f7b245571ca
Rilanitech <ilanirejoice50@gmail.com>
2023-02-17 14:30:36 -0800

    Numbers2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{	char i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
