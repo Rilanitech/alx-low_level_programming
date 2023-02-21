0817847de31cafb815691c614bda96522952f79f
Rilanitech <ilanirejoice50@gmail.com>
2023-02-21 05:47:17 -0800

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
