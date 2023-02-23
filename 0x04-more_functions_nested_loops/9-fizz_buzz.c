22cb5673469ddfd44e2af3c69bf6833af60bc35b
Rilanitech <ilanirejoice50@gmail.com>
2023-02-23 14:25:46 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,22 +6,19 @@
  */
 int main(void)
 {
-	int x;
-
-	for (x = 1; x <= 100; x++)
-	{
-		if (x == 100)
-			printf("Buzz ");
-		if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz ");
-		else if (x % 3 == 0)
-			printf("Fizz ");
-		else if (x % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d ", x);
-	}
-	printf("\n");
-	return (0);
+int i;	for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%i", i);
+if (i < 100)
+printf(" ");
 }
-
+printf("\n");
+return (0);
+}
