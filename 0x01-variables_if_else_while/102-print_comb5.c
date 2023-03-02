7fb9c2a7131efc318fc9ac8e033f6901b3f6e893
Rilanitech <ilanirejoice50@gmail.com>
2023-03-01 21:51:32 -0800

    strcat

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,22 +5,35 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int c, i, k, j;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (c = 49; c <= 57; c++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (i = 48; i <= 57;  i++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 90 && num2 == 99)
-				continue;
-			putchar(' ');
-			putchar(' ');
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if ((k+j) > (c + i) && k >= c) || ( c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+						if (c + i + k + j == 227 && c == 57)
+						{
+							break;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
 		}
 	}
 	putchar('\n');
